c5af22bcaa6ba0915c6761cfc899a3b65166ff80
circulardark <circulardark@gmail.com>
2014-04-29 21:04:17 -0400

    fmt.sh added

---
+++ chubby_test/3s3c_normal/node3/node.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gobby/src/chubbyserver"
+	"github.com/gobby/src/config"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+const (
+	nid      = 2
+	numNodes = 3
+)
+
+func main() {
+	fmt.Printf("server %d starts\n", nid)
+	_, err := chubbyserver.NewChubbyServer(nid, numNodes)
+	if err != nil {
+		fmt.Println("Cannot start node.\n")
+		fmt.Println(err)
+		return
+	}
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Nodes[nid].Port))
+	if err != nil {
+		fmt.Printf("node %d cannot listen to port:%s\n", err)
+		return
+	}
+	rpc.HandleHTTP()
+	http.Serve(listener, nil)
+}
--- chubby_test/3s3c_normal/run.sh
+++ chubby_test/3s3c_normal/run.sh
@@ -2,5 +2,7 @@ rm -f dumplog_*
 go run node1/node.go &
 go run node2/node.go &
 go run node3/node.go &
-go run node4/node.go &
-go run node5/node.go &
+sleep 3
+go run c1/client.go &
+go run c2/client.go &
+go run c3/client.go &
